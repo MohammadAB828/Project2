0da517997ed46d3da9dde26b72f5a27bb2517c7f
MohammadAB828 <mohammad.abedi5513@gmail.com>
2024-05-16 16:41:03 +0330

    create Person in Header.cpp

--- Project2/Header.cpp
+++ Project2/Header.cpp
@@ -1,3 +1,33 @@
 #include "Header.h"
 
 
+Person::Person()
+{
+}
+
+Person::~Person()
+{
+
+}
+
+void Person::set_id(string ID)
+{
+	id = ID;
+}
+
+void Person::set_password(string Pass)
+{
+	password = Pass;
+}
+
+string Person::get_id()
+{
+	return id;
+}
+
+string Person::get_password()
+{
+	return password;
+}
+
+
