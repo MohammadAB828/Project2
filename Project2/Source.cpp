db116cae8ecfd21eac51850c8359e5adf6083997
MohammadAB828 <mohammad.abedi5513@gmail.com>
2024-05-16 16:32:12 +0330

    update int main() in source file

--- Project2/Source.cpp
+++ Project2/Source.cpp
@@ -1,2 +1,34 @@
 #include <limits>
 #include "Header.h"
+
+int main()
+{
+
+	while (true)
+	{
+
+	menu:
+		system("cls");
+		cout << "WELCOME\n\n" << "1.Login\n" << "2.Sign in\n" << "3.Exit\n";
+
+		int Entry_Number;
+		cin >> Entry_Number;
+
+		switch (Entry_Number)
+		{
+			case 3:
+			{
+				system("cls");
+				cout << "Good Luck";
+				Sleep(3000);
+				system("cls");
+				break;
+			}
+
+			default:
+				goto menu;
+				break;
+		}
+
+	}
+}
