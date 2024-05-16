276a7e348dd67d81400f4199d1862d930f0f8f14
MohammadAB828 <mohammad.abedi5513@gmail.com>
2024-05-16 16:40:03 +0330

    create Person class

--- Project2/Header.h
+++ Project2/Header.h
@@ -6,3 +6,19 @@
 #include <string.h>
 using namespace std;
 
+class Person
+{
+private:
+	string id;
+	string password;
+public:
+	Person();
+	~Person();
+	string first_name;
+	string last_name;
+	bool is_admin;
+	void set_id(string);
+	void set_password(string);
+	string get_id();
+	string get_password();
+};
