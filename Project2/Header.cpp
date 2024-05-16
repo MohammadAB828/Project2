cddcea16dfd3c1cf1babb68cb589f2394a23509a
MohammadAB828 <mohammad.abedi5513@gmail.com>
2024-05-16 16:51:21 +0330

    update Header.cpp file

--- Project2/Header.cpp
+++ Project2/Header.cpp
@@ -43,6 +43,57 @@ Teacher::~Teacher()
 }
 
 
+istream& operator>>(istream& input, Teacher& obj) {
+
+	string first_name, last_name, password, re_password, id;
+	cout << "Id : ";
+	input >> id;
+	cout << "First Name : ";
+	input >> first_name;
+	cout << "Last Name : ";
+	input >> last_name;
+password:
+	cout << "Password : ";
+	input >> password;
+	cout << "Repeat Password : ";
+	input >> re_password;
+
+	if (password == re_password) {
+		obj.set_id(id);
+		obj.first_name = first_name;
+		obj.last_name = last_name;
+		obj.set_password(password);
+	}
+	else {
+		cout << "password is not verified\n";
+		Sleep(3000);
+		system("cls");
+		cout << "Id : ";
+		cout << id << endl;
+		cout << "First Name : ";
+		cout << first_name << endl;
+		cout << "Last Name : ";
+		cout << last_name << endl;
+		goto password;
+	}
+
+	delete_last_line("teacher.json");
+
+	//write
+	Json::Value x, y;
+	x["id"] = obj.get_id();
+	x["first_name"] = obj.first_name;
+	x["last_name"] = obj.last_name;
+	x["password"] = obj.get_password();
+	y = x;
+
+	write_object(y, "teacher.json");
+
+	return input;
+}
+
+
+
 //////
 
 
